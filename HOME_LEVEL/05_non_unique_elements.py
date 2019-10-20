78e0451892aefde0d3918dc6672cc149e0d102ce
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-10-21 00:33:19 +0530

    non-unique elements
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ HOME_LEVEL/05_non_unique_elements.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""
+Purpose: https://py.checkio.org/mission/non-unique-elements/solve/
+"""
+
+def checkio(data: list) -> list:
+    return [ele for ele in data if data.count(ele) > 1]
+
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert list(checkio([1, 2, 3, 1, 3])) == [1, 3, 1, 3], "1st example"
+    assert list(checkio([1, 2, 3, 4, 5])) == [], "2nd example"
+    assert list(checkio([5, 5, 5, 5, 5])) == [5, 5, 5, 5, 5], "3rd example"
+    assert list(checkio([10, 9, 10, 10, 9, 8])) == [10, 9, 10, 10, 9], "4th example"
+    print("It is all good. Let's check it now")
