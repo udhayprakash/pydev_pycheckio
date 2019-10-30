db723c167b208f07dbf91e98fc176aeb86dd63c2
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-10-30 21:56:33 +0530

    three words problem
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ 01_Elementary/17_three_words.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+"""
+Purpose: https://py.checkio.org/mission/three-words/solve/
+"""
+
+
+def checkio(words: str) -> bool:
+    count = 0
+    for word in words.split():
+        if word.isalpha():
+            count += 1
+            if count == 3:
+                return True
+        else:
+            count = 0
+    return False
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(checkio("Hello World hello"))
+
+    assert checkio("Hello World hello") == True, "Hello"
+    assert checkio("He is 123 man") == False, "123 man"
+    assert checkio("1 2 3 4") == False, "Digits"
+    assert checkio("bla bla bla bla") == True, "Bla Bla"
+    assert checkio("Hi") == False, "Hi"
+    assert checkio("one two 3 four five six 7 eight 9 ten eleven 12") == True, "big"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
