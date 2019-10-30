b136dc1fdac46d70f700bfa4d2b32e24b298f1d2
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-10-30 08:54:08 +0530

    second index problem
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ 01_Elementary/12_second_index.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""
+Purpose: https://py.checkio.org/mission/second-index/solve/
+"""
+
+
+def second_index(text: str, symbol: str) -> [int, None]:
+    """
+        returns the second index of a symbol in a given text
+    """
+    result = text.find(symbol, text.find(symbol))
+    return result if result != -1 else None
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(second_index("sims", "s"))
+
+    assert second_index("sims", "s") == 3, "First"
+    assert second_index("find the river", "e") == 12, "Second"
+    assert second_index("hi", " ") is None, "Third"
+    assert second_index("hi mayor", " ") is None, "Fourth"
+    assert second_index("hi mr Mayor", " ") == 5, "Fifth"
+    print('You are awesome! All tests are done! Go Check it!')
