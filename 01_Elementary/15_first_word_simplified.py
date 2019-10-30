6f97248c494e3ad7acc064651ed1f788306833e6
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-10-30 21:56:05 +0530

    first word problem
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ 01_Elementary/15_first_word_simplified.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+"""
+Purpose: https://py.checkio.org/mission/first-word-simplified/solve/
+"""
+
+
+def first_word(text: str) -> str:
+    """
+        returns the first word in a given text.
+    """
+    return text.split(' ')[0]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(first_word("Hello world"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert first_word("Hello world") == "Hello"
+    assert first_word("a word") == "a"
+    assert first_word("hi") == "hi"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
