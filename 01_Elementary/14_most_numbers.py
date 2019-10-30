61721b32fe1867c665e692f12acba81e227ae586
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-10-30 09:23:26 +0530

    most number problem
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ 01_Elementary/14_most_numbers.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+"""
+Purpose: https://py.checkio.org/mission/most-numbers/solve/
+"""
+
+
+def checkio(*args):
+    return (max(args) - min(args)) if args else 0
+
+
+if __name__ == '__main__':
+    def almost_equal(checked, correct, significant_digits):
+        precision = 0.1 ** significant_digits
+        return correct - precision < checked < correct + precision
+
+
+    print('Example:')
+    print(checkio(1, 2, 3))
+    print(almost_equal(checkio(1, 2, 3), 2, 3))
+
+    assert almost_equal(checkio(1, 2, 3), 2, 3), "3-1=2"
+    assert almost_equal(checkio(5, -5), 10, 3), "5-(-5)=10"
+    assert almost_equal(checkio(10.2, -2.2, 0, 1.1, 0.5), 12.4, 3), "10.2-(-2.2)=12.4"
+    assert almost_equal(checkio(), 0, 3), "Empty"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
