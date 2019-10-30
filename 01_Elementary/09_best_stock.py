5f35b101f93e08acef2eb1367354cb5337ed2b52
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-10-30 08:36:09 +0530

    best stock problem
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ 01_Elementary/09_best_stock.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+"""
+Purpose: https://py.checkio.org/mission/best-stock/solve/
+"""
+
+
+def best_stock(a):
+    return sorted(a.items(), key=lambda x: x[1])[-1][0]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(best_stock({"CAC": 10.0, "ATX": 390.2, "WIG": 1.2}))
+
+    assert best_stock({"CAC": 10.0, "ATX": 390.2, "WIG": 1.2}) == "ATX"
+    assert best_stock({"CAC": 91.1, "ATX": 1.01, "TASI": 120.9}) == "TASI"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
