ae2640e9d03615810d275e97434f60f5294156f9
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-11-01 00:18:16 +0530

    median problem
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ 05_Oreilly/01_median.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""
+Purpose: https://py.checkio.org/en/mission/median/
+"""
+from typing import List
+
+
+def checkio(data: List[int]) -> [int, float]:
+    data.sort()
+    middle_index, odd_ness = divmod(len(data), 2)
+    if odd_ness:  # For odd no. of elements
+        return data[middle_index]
+    return (data[middle_index] + data[middle_index - 1]) / 2
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(checkio([1, 2, 3, 4, 5]))
+
+    assert checkio([1, 2, 3, 4, 5]) == 3, "Sorted list"
+    assert checkio([3, 1, 2, 5, 3]) == 3, "Not sorted list"
+    assert checkio([1, 300, 2, 200, 1]) == 2, "It's not an average"
+    assert checkio([3, 6, 20, 99, 10, 15]) == 12.5, "Even length"
+    print("Start the long test")
+    assert checkio(list(range(1000000))) == 499999.5, "Long."
+    print("Coding complete? Click 'Check' to earn cool rewards!")
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # pydev_incubation_D2_D5
 
 https://py.checkio.org/station/home/
+
+https://py.checkio.org/station/oreilly/
