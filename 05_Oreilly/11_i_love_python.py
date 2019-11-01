39c56db728b2691b25a872d47b0883b60941b256
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-11-01 08:53:40 +0530

    I love python
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

---
+++ 05_Oreilly/11_i_love_python.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""
+Purpose: https://py.checkio.org/mission/i-love-python/solve/
+"""
+
+
+def i_love_python():
+    """
+        Let's explain why do we love Python.
+    """
+    return "I love Python" + '!'
+
+
+if __name__ == '__main__':
+    assert i_love_python() == "I love Python!"
